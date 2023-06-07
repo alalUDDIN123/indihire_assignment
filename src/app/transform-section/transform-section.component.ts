7eb05cccfae97aa45ea80ae0014b893108708332
alalUDDIN123 <alalu75666@gmail.com>
2023-06-07 10:54:37 +0530

    added_comment_in_transaction_componet.ts

--- src/app/transform-section/transform-section.component.ts
+++ src/app/transform-section/transform-section.component.ts
@@ -9,20 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./transform-section.component.scss']
 })
 export class TransformSectionComponent {
+  // Define faArrowRight variable
   faArrowRight = faArrowRight;
 
+  // Define message object
   message: TransformerMessage = {
     heading: 'Transforming organizations, driving value',
     message: "People are the core to our every team, business, and process. We bring value by optimally combining the art and science of finding the right talent and the right role. Our streamlined processes and understanding of industry dynamics empower us to efficiently solve talent and recruitment problems.",
     redirectLink: ["https://indihire.com/what-we-do", "https://indihire.com/navigate-to-your-next-opportunity/#job-opportunities"]
   };
 
-  behindImages: BehindImages = ["../../assets/images/left_row.jpg", "../../assets/images/right_hand_row.jpg"]
+  // Define behindImages array
+  behindImages: BehindImages = ["../../assets/images/left_row.jpg", "../../assets/images/right_hand_row.jpg"];
 
+  // Define ArrowImage variable
+  ArrowImage: string = "../../assets/images/straight-arrow.png";
 
-  ArrowImage: string = "../../assets/images/straight-arrow.png"
-
-
+  // Define swiperData array
   swiperData: SwiperData[] = [
     {
       title: "RPO",
@@ -38,46 +41,61 @@ export class TransformSectionComponent {
     }
   ];
 
-  slidePosition = 0;
+  // Initialize currentIndex variable
+  currentIndex = 0;
+
+  // Initialize autoplayInterval variable
   autoplayInterval: any;
 
+  // Called when the component is initialized
   ngOnInit() {
+    this.insertNextImage();
     this.startAutoplay();
   }
 
+  // Called when the component is destroyed
   ngOnDestroy() {
     this.stopAutoplay();
   }
 
+  // Inserts the next image in the carousel
+  insertNextImage() {
+    // Get carousel elements by their IDs
+    const image = document.getElementById("carouselImage") as HTMLImageElement;
+    const title = document.getElementById("carouselTitle") as HTMLElement;
+
+    // Set the image source and title from the swiperData
+    image.src = this.swiperData[this.currentIndex].image;
+    title.textContent = this.swiperData[this.currentIndex].title;
+
+    // Increment the index
+    this.currentIndex = (this.currentIndex + 1) % this.swiperData.length;
+  }
+
+  // Moves to the previous slide
   previousSlide() {
-    if (this.slidePosition === 0) {
-      this.slidePosition = (this.swiperData.length - 1) * -100;
-    } else {
-      this.slidePosition += 100;
-    }
+    this.currentIndex = (this.currentIndex - 1 + this.swiperData.length) % this.swiperData.length;
+    this.insertNextImage();
   }
 
+  // Moves to the next slide
   nextSlide() {
-    if (this.slidePosition === (this.swiperData.length - 1) * -100) {
-      this.slidePosition = 0;
-    } else {
-      this.slidePosition -= 100;
-    }
+    this.insertNextImage();
   }
 
-
-
+  // Starts the autoplay of the carousel
   startAutoplay() {
     this.autoplayInterval = setInterval(() => {
       this.nextSlide();
-    }, 2000); // here we can maintain time delay of carousal
+    }, 2000);
   }
 
+  // Stops the autoplay of the carousel
   stopAutoplay() {
     clearInterval(this.autoplayInterval);
   }
 
-
+  // Define transformSectionData object
   transformSectionData: TransformSectionData = {
     transformerMessage: this.message,
     swiperData: this.swiperData,
